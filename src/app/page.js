64432acb81636f15f5152cc965740a1571e0d10e
Raneemoqaily7 <raneemoqaily7@gmail.com>
2025-01-13 01:03:53 +0300

    editing the pagination is done

--- src/app/page.js
+++ src/app/page.js
@@ -6,31 +6,28 @@ import CardList from "@/components/cardList/CardList";
 import CategoryList from "@/components/categoryList/CategoryList";
 import Menu from "@/components/Menu/Menu";
 import styles from "./home.module.css"
-export default function Home({searchParams}) {
-
-
-  const page = parseInt(searchParams.page) || 1;
+export default function Home() {
   return (
 
-    <div className={styles.container}>
-      <Feature />
-      <CategoryList />
+<div className= {styles.container}>
+  <Feature/>
+  <CategoryList/>
+  
 
+  
+  <div className={styles.content}>
+    <CardList/>
+    <Menu/>
 
-
-      <div className={styles.content}>
-        <CardList page={page} />
-        <Menu />
-
-      </div>
-    </div>
+  </div>
+</div>
   )
-
-
-
-
-
-
-
-    ;
+    
+  
+    
+    
+        
+        
+    
+  ;
 }
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -4,25 +4,7 @@ import styles from "./cardList.module.css";
 import Card from "../CardDetails/Card";
 
 
-
-
-const getData = async (page) => {
-    const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
-      cache: "no-store",
-    });
-    if (!res.ok) {
-      throw new Error("Failed");
-    }
-  
-    return res.json();
-  };
-
-const CardList = async ({page}) => {
-
-     const data= await getData(page)
-     const Post_PER_PAGE = 2;
-     
-
+const CardList = () => {
     
     return (
         <div className={styles.container}>
@@ -30,10 +12,10 @@ const CardList = async ({page}) => {
             <h1 className={styles.title}> Recent Blogs</h1>  
                 <div className={styles.blogs}>
                     <div className={styles.blog}>
-                        {data?.map((item)=>(
-                        <Card item={item} key={item._id}/>
-                        ))}
-                       
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
 
                         {/* <div className={styles.imageContainer}>
                             <Image src="p1.png" alt="blog" fill />
@@ -43,7 +25,7 @@ const CardList = async ({page}) => {
                 </div>
           
 
-            <Pagination  page={page} hasPrev={hasPrev} hasNext={hasNext}/>
+           
         </div>
 
     )
