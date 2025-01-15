96ad0e0578f4c95301c1448a286d7170564e64ec
Raneemoqaily7 <raneemoqaily7@gmail.com>
2025-01-15 16:57:30 +0300

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true,
+      },
+      env: {
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL || "http://localhost:3000",
+      },
     images:{
         domains: ["lh3.googleusercontent.com"]
     }
