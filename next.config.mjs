13d9190ed051818f6ca495f99d79560c8fcd0fe4
Raneemoqaily7 <raneemoqaily7@gmail.com>
2025-01-15 18:15:45 +0300

    edit env

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-      },
+   
       env: {
         NEXTAUTH_URL: process.env.NEXTAUTH_URL || "http://localhost:3000",
+        GOOGLE_ID: process.env.GOOGLE_ID,
+        GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+        DATABASE_URL: process.env.DATABASE_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
       },
     images:{
         domains: ["lh3.googleusercontent.com"]
